53f5aff2e0f368f071c9b00f0a723e26752fcb81
IsaevPVL <61705015+IsaevPVL@users.noreply.github.com>
2021-12-20 13:21:12 +0500

    new2

--- Assets/Scripts/Dragging.cs
+++ Assets/Scripts/Dragging.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dragging : MonoBehaviour
-{   
+{
     float distanceZ;
     bool isDragging = false;
     Vector3 offset;
@@ -15,32 +15,40 @@ public class Dragging : MonoBehaviour
     float widthMin;
     float widthMax;
 
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
     }
 
-    void Start() {
-        width = 1/(cam.WorldToViewportPoint(new Vector3(1, 1, 0)).x - 0.5f);
-        widthMin = -width/2f + .8f;
-        widthMax = width/2f - .8f;
+    void Start()
+    {
+        width = 1 / (cam.WorldToViewportPoint(new Vector3(1, 1, 0)).x - 0.5f);
+        widthMin = -width / 2f + .8f;
+        widthMax = width / 2f - .8f;
     }
 
     void Update()
     {
         Vector3 moveTo;
 
-        if(Input.touchCount == 0){
+        if (Input.touchCount == 0)
+        {
             isDragging = false;
-        }else{
+        }
+        else
+        {
             Touch touch = Input.touches[0];
             Vector3 touchPosition = touch.position;
 
-            if(touch.phase == TouchPhase.Began){
+            if (touch.phase == TouchPhase.Began)
+            {
                 Ray ray = cam.ScreenPointToRay(touchPosition);
                 RaycastHit hit;
-                if(Physics.Raycast(ray, out hit)){
-                    if(hit.collider.tag == "Paddle"){
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.tag == "Paddle")
+                    {
                         toDrag = hit.transform;
                         distanceZ = hit.transform.position.z - Camera.main.transform.position.z;
                         //moveTo = new Vector3(touchPosition.x, touchPosition.y, distanceZ);
@@ -52,7 +60,8 @@ public class Dragging : MonoBehaviour
                 }
             }
 
-            if(isDragging && touch.phase == TouchPhase.Moved){
+            if (isDragging && touch.phase == TouchPhase.Moved)
+            {
                 //moveTo = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distanceZ);
                 moveTo = new Vector3(touchPosition.x, toDrag.position.y, distanceZ);
                 moveTo = cam.ScreenToWorldPoint(moveTo) + offset;
@@ -62,7 +71,8 @@ public class Dragging : MonoBehaviour
                 rb.MovePosition(moveTo);
             }
 
-            if(isDragging && (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended)){
+            if (isDragging && (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended))
+            {
                 isDragging = false;
             }
         }
